23eb081c5da9a267a84a067b6fbf2acddd9250f7
denis_variukhin <theeldergodzagrej@yandex.ru>
2023-09-21 17:41:08 +0300

    апдейт с 4 по 6 модуль

--- Module15/06_running_nums/main.py
+++ Module15/06_running_nums/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+N = int(input("Введите кол-во элементов табло: "))
+K = int(input("Введите значение сдвига: "))
+nums = []
+count = N
+
+for i in range(1, N+ 1):
+    num = int(input(f"Введите {i} цифру которая должна быть на табло: "))
+    nums.append(num)
+
+for index, number in enumerate(nums):
+    print(nums[index])
+

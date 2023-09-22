794482bbab521555fe3b7ba294b90414fb20726b
denis_variukhin <theeldergodzagrej@yandex.ru>
2023-09-22 17:42:21 +0300

    изменения по 7 и 9 задаче, полностью убрал списки из 7 задания.

--- Module15/07_word_analysis_2/main.py
+++ Module15/07_word_analysis_2/main.py
@@ -1,19 +1,6 @@
-check_palindrome = list(input("Введите слово для проверки: "))
-check_palindrome_2 = []
-check_palindrome_2.append(check_palindrome[::-1])
+check_palindrome = input("Введите слово для проверки: ")
 
-if check_palindrome == check_palindrome_2[0]:
-    print("Да это палиндром")
+if check_palindrome == check_palindrome[::-1]:
+    print("Да это палиндром!")
 else:
-    print("Нет это не палиндром")
-
-# print(check_palindrome)
-# count = 0
-#
-# for index, sym in enumerate(check_palindrome):
-#     if check_palindrome[index] == check_palindrome[-index - 1]:
-#         count += 1
-#     if count == len(check_palindrome):
-#         print("Да это палиндром")
-#     else:
-#         print("Нет этон е палиндром")
+    print("Нет это не палиндром!")
--- Module15/09_reverse_analysis/main.py
+++ Module15/09_reverse_analysis/main.py
@@ -2,14 +2,21 @@ import random
 # numbers_list = [7, 14, 3, 18, 21, 10, 9, 6]
 numbers_list = []
 max_index = 0
+index_in_numlist = 0
 for _ in range(random.randrange(1, 15)):
     numbers_list.append(random.randint(1, 99))
 print(f"Изначальный список: ", *numbers_list)
 
-for index, num in enumerate(numbers_list):
-    if num % 2 != 0:
-        numbers_list.remove(num)
-    max_index = len(numbers_list) - 1
+
+while index_in_numlist < len(numbers_list):
+    if numbers_list[index_in_numlist] % 2 != 0:
+        numbers_list.pop(index_in_numlist)
+    else:
+        index_in_numlist += 1
+# for index, num in enumerate(numbers_list):
+#     if num % 2 != 0:
+#         numbers_list.remove(num)
+#     max_index = len(numbers_list) - 1
 
 for i in range(max_index):
     swap_num = numbers_list.pop(max_index)

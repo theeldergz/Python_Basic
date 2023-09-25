17a8f4751f4e104b9180e06f6f25daf3ae527a2f
Denis Varjuhin <denisvaruxin@mail.ru>
2023-09-25 17:18:18 +0300

    апдейт по 6-8 задаче 16 модуля

--- Module16/07_rhyme_cnt/main.py
+++ Module16/07_rhyme_cnt/main.py
@@ -1,1 +1,18 @@
-# TODO здесь писать код
+N = int(input("Кол-во игроков: "))
+K = int(input("Какое число в считалке?  "))
+player_list = list(range(1, N + 1))
+
+for _ in range(N - 1):
+    print("Текущий круг людей: ", player_list)
+    num = int(input("Начало счета с номера: "))
+
+    lenght = len(player_list)
+    dash = K % lenght
+    index = player_list.index(num)
+    del_human = player_list[index + dash - 1]
+    player_list.remove(del_human)
+
+    print("Выбыл ", del_human)
+
+print("\n\tПобедил ", *player_list)
+

c287ef47a70767eb6f449d4be49c65ad593d7894
Denis Varjuhin <denisvaruxin@mail.ru>
2023-09-28 15:30:21 +0300

    изменения по 5 - 8 задаче

--- Module17/06_two_dimensional_list/main.py
+++ Module17/06_two_dimensional_list/main.py
@@ -1,1 +1,6 @@
-# TODO здесь писать код
+a = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]
+print(a[2])
+
+search2d = [[num[i] for num in a] for i in range(4)]
+
+print(search2d)
--- Module17/07_list_of_lists_2/main.py
+++ Module17/07_list_of_lists_2/main.py
@@ -1,4 +1,8 @@
-nice_list = [[[1, 2, 3], [4, 5, 6], [7, 8, 9]],
-             [[10, 11, 12], [13, 14, 15], [16, 17, 18]]]
+nice_list = [[[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[10, 11, 12], [13, 14, 15], [16, 17, 18]]]
 
-# TODO здесь писать код
+answer = [nice_list[index1][index2][index3]
+          for index1 in range(len(nice_list))
+          for index2 in range(len(nice_list[index1]))
+          for index3 in range(len(nice_list[index2 - 1]))]
+
+print(answer)

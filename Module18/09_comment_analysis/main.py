3a3865a33c66ecc2f2cad699b184a69058279476
Skillbox <it@skillbox.ru>
2023-09-29 17:20:03 +0300

    Add lesson

---
+++ Module18/09_comment_analysis/main.py
@@ -0,0 +1,8 @@
+# TODO здесь писать код
+
+
+# Пример использования:
+text = input("Введите строку для анализа: ")
+uppercase, lowercase = count_uppercase_lowercase(text)
+print("Количество заглавных букв:", uppercase)
+print("Количество строчных букв:", lowercase)

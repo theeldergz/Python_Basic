ea4e378d7ddcbf861010b942ee8ec520be786437
Denis Varjuhin <denisvaruxin@mail.ru>
2023-10-04 16:51:16 +0300

    изменения по 3 му заданию

--- Module18/03_files/main.py
+++ Module18/03_files/main.py
@@ -1,22 +1,13 @@
-banned_symbols = "@№$%^&\*()"
-banned_extensions = [".txt", ".docx"]
+banned_symbols = ("@№$%^&\*()")
+banned_extensions = (".txt", ".docx")
 flag_banned_sym = False
 flag_banned_ext = True
 
 user_input = input("Введите название файла: ")
 
-for sym in banned_symbols:
-    if user_input.startswith(sym):
-        flag_banned_sym = True
-
-for ext in banned_extensions:
-    if user_input.endswith(ext):
-        flag_banned_ext = False
-
-
-if flag_banned_sym:
-    print("Ошибка: название начинается на один из специальных символов.")
-elif flag_banned_ext:
-    print("Ошибка: неверное расширение файла. Ожидалось .txt или .docx.")
+if user_input.startswith(banned_symbols):
+    print('Ошибка: название начинается на один из специальных символов.')
+elif not user_input.endswith(banned_extensions):
+    print('Ошибка: неверное расширение файла. Ожидалось .txt или .docx.')
 else:
     print("Файл назван верно.")

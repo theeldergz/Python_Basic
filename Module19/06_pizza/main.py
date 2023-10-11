6253450a55a5e0133a4c902063d59393c7737594
Denis Varjuhin <denisvaruxin@mail.ru>
2023-10-12 00:25:49 +0300

    апдейт 6-7 задания

--- Module19/06_pizza/main.py
+++ Module19/06_pizza/main.py
@@ -1,1 +1,27 @@
-# TODO здесь писать код
+guests_orders = {}
+
+
+
+order_count = int(input("Введите колво заказов: "))
+
+for index_info in range(order_count):
+    order = input(f"Введите {index_info + 1} заказ(через пробел): ").lower().split()
+    order[2] = int(order[2])
+    if order[0] not in guests_orders:
+        guests_orders[order[0]] = {order[1]: order[2]}
+    elif order[1] not in guests_orders[order[0]]:
+        guests_orders[order[0]][order[1]] = order[2]
+    else:
+        guests_orders[order[0]][order[1]] += order[2]
+
+guests_orders_name = [key for key in guests_orders.keys()]
+guests_orders_name.sort()
+
+
+for guest in guests_orders_name:
+    guests_orders_pizza = [key for key in guests_orders[guest].keys()]
+    print(guest.capitalize(), ": ")
+    guests_orders_pizza.sort()
+    for pizza in guests_orders_pizza:
+        print("\t", pizza.capitalize(), ": ", guests_orders[guest][pizza])
+
--- Module19/07_three_lists/main.py
+++ Module19/07_three_lists/main.py
@@ -1,1 +1,28 @@
-# TODO здесь писать код
+array_1 = [1, 5, 10, 20, 40, 80, 100]
+array_2 = [6, 7, 20, 80, 100]
+array_3 = [3, 4, 15, 20, 30, 70, 80, 120]
+array_x = []
+
+
+for num in array_1:
+    if num in array_2 and num in array_3:
+       array_x.append(num)
+
+print("Задача номер 2")
+print("\n\tРешение без set: ", *array_x)
+print("\tРешение с set: ", *set(array_1) & set(array_2) & set(array_3))
+
+
+array_x.clear()
+for num in array_1:
+    if num not in array_2 and num not in array_3:
+       array_x.append(num)
+
+array_1_set = set(array_1)
+array_1_set -= set(array_2)
+array_1_set -= set(array_3)
+
+print("\nЗадача номер 1")
+print("\n\tРешение без set: ", *array_x)
+print("\tРешение с set: ", *array_1_set)
+

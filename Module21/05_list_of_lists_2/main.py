d8d87b5d33bb48dddcb7456fd324a4e43bc1684c
Denis Varjuhin <denisvaruxin@mail.ru>
2023-10-22 15:30:30 +0300

    апдейт по 5 заданию

--- Module21/05_list_of_lists_2/main.py
+++ Module21/05_list_of_lists_2/main.py
@@ -1,14 +1,26 @@
+import copy
+
 nice_list = [1, 2, [3, 4], [[5, 6, 7], [8, 9, 10]],
              [[11, 12, 13], [14, 15], [16, 17, 18]]]
 
 
-def unpack(some_list, summary=[]):
+def unpacker(some_list, summary=[]):
     for elem in some_list:
         if isinstance(elem, list):
-            unpack(elem)
+            unpacker(elem)
         else:
             summary.append(elem)
     return summary
 
 
+
+
+def unpack(test_list):
+    some_list = unpacker(test_list)
+    second_list = some_list
+    result_list = copy.deepcopy(second_list)
+    second_list.clear()
+    return result_list
+
+print(unpack(nice_list))
 print(unpack(nice_list))

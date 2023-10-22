44a99401e5e5209faf25ff33019242e2527bc8fb
Denis Varjuhin <denisvaruxin@mail.ru>
2023-10-22 18:11:16 +0300

    апдейт по 3 заданию

--- Module21/03_deep_copy/main.py
+++ Module21/03_deep_copy/main.py
@@ -15,16 +15,23 @@ site = {
 
 
 def site_copy(site_reference, name_product):
-    new_site = copy.deepcopy(site)
-    new_site['html']['head']['title'] = f'Куплю/продам {name_product} недорого'
-    new_site['html']['body']['h2'] = f'У нас самая низкая цена на {name_product}'
-    return new_site
+    second_site = copy.deepcopy(site_reference)
+    second_site['html']['head']['title'] = f'Куплю/продам {name_product} недорого'
+    second_site['html']['body']['h2'] = f'У нас самая низкая цена на {name_product}'
+    return second_site
 
 
 site_count = int(input("Сколько сайтов: "))
 
+new_sites = {}
+
 for _ in range(site_count):
     phone_name = input("Введите название продукта для нового сайта: ")
-    second_site = site_copy(site, phone_name)
-    print(f'Сайт для {phone_name}: ')
-    pprint.pprint(second_site)
+    site_header = f'Сайт для {phone_name}'
+    new_sites[site_header] = site_copy(site, phone_name)
+    for key, value in new_sites.items():
+        print(key)
+        pprint.pprint(value)
+
+
+

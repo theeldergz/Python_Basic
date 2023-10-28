c6b79a71c3d8822b1de4eda3c29b1d359e57d631
Skillbox <it@skillbox.ru>
2023-10-28 18:24:19 +0300

    Add lesson

---
+++ Module23/05_reliable_calculations/main.py
@@ -0,0 +1,10 @@
+# TODO здесь писать код
+
+# Здесь создайте функцию get_sage_sqrt
+
+
+# Тестовые случаи
+numbers = [16, 25, -9, 0, 4.5, "abc"]
+for number in numbers:
+    result = get_sage_sqrt(number)
+    print(f"Квадратный корень numbers {number}: {result}")

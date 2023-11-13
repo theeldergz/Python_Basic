28a60278d4e1b0da68e4be9bf42aea460f817549
Skillbox <it@skillbox.ru>
2023-11-13 17:53:12 +0300

    Add lesson

---
+++ Module25/06_Abstract_class/main.py
@@ -0,0 +1,19 @@
+# TODO здесь писать код
+
+
+
+# Примеры работы с классом:
+# Создание экземпляров классов
+circle = Circle(5)
+rectangle = Rectangle(4, 6)
+triangle = Triangle(3, 8)
+
+# Вычисление площади фигур
+circle_area = circle.area()
+rectangle_area = rectangle.area()
+triangle_area = triangle.area()
+
+# Вывод результатов
+print("Площадь круга:", circle_area)
+print("Площадь прямоугольника:", rectangle_area)
+print("Площадь треугольника:", triangle_area)

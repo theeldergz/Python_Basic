d5b8672963fad80189826067cf8e2fa76017eb59
Denis Varjuhin <denisvaruxin@mail.ru>
2023-11-20 18:01:30 +0300

    апдейт по 2-3 заданию

--- Module25/02_karma/main.py
+++ Module25/02_karma/main.py
@@ -1,26 +1,50 @@
 import random
 
-EXC_LIST = ['KillError', 'DrunkError', 'CarCrashError', 'GluttonyError', 'DepressionError', None]
+
+class KillError(Exception):
+    def __init__(self):
+        super().__init__('Вы убили человека!')
+
+
+class DrunkError(Exception):
+    def __init__(self):
+        super().__init__('Вы напились!')
+
+
+class CarCrashError(Exception):
+    def __init__(self):
+        super().__init__('Вы попали в дтп!')
+
+
+class GluttonyError(Exception):
+    def __init__(self):
+        super().__init__('Вы обьелись!')
+
+
+class DepressionError(Exception):
+    def __init__(self):
+        super().__init__('Депрессия...')
+
+
 NEED_KARMA = 0
 
 
 def one_day():
-    data = random.choices(EXC_LIST, weights=[0.02, 0.02, 0.02, 0.02, 0.02, 0.9])
+    exceptions = random.choice((KillError, DrunkError, CarCrashError, GluttonyError, DepressionError))
     try:
-        if data[0] is None:
-            return random.randint(1, 7)
+        if random.randint(1, 10) == 7:
+            raise exceptions
         else:
-            with open('karma.log', 'a') as karma_logs:
-                line = '\n' + str(ValueError(data[0]))
-                karma_logs.write(line)
-                raise ValueError(data[0])
+            return random.randint(1, 7)
 
-    except ValueError:
-        return 0
+    except (KillError, DrunkError, CarCrashError, GluttonyError, DepressionError) as exc:
+        print(exc)
 
 
 while NEED_KARMA <= 500:
-    NEED_KARMA += one_day()
-
-
-
+    try:
+        NEED_KARMA += one_day()
+    except TypeError:
+        pass
+else:
+    print('\nВы успешно прожили жизнь не смотря на все неудачи!!!')
--- Module25/03_my_dict/main.py
+++ Module25/03_my_dict/main.py
@@ -1,47 +1,51 @@
-class MyDict:
-    def __init__(self):
-        self.__key_list = list()
-        self.__value_list = list()
+class MyDict(dict):
+    def get(self, key, default=0):
+        return super().get(key, default)
 
-    def __str__(self):
-        __line = []
-        for i, _ in enumerate(self.__key_list):
-            __line.append(str(self.__key_list[i]) + ': ' + str(self.__value_list[i]))
-        line = " ".join(__line)
-
-        return '{' + line + '}'
-
-    def update(self, external_key, external_value):
-        if not isinstance(external_key, (tuple, frozenset, str, int, float, bool)):
-            raise TypeError
-        else:
-            if external_key not in self.__key_list:
-                self.__key_list.append(external_key)
-                self.__value_list.append(external_value)
-            else:
-                index = self.__key_list.index(external_key)
-                self.__value_list[index] = external_value
-
-    def get(self, external_key, external_value=None):
-        if not isinstance(external_key, (tuple, frozenset, str, int, float, bool)):
-            raise TypeError
-        else:
-            if external_key not in self.__key_list:
-                return 0
-            else:
-                self.__key_list.append(external_key)
-                index = self.__key_list.index(external_key)
-                return self.__value_list[index]
-
-
-dict_test = MyDict()
-print(dict_test)
-dict_test.update('String1', 10)
-print(dict_test)
-dict_test.update('String2', 14)
-dict_test.update('String3', 1235)
-print(dict_test)
-key_true = dict_test.get('String2')
-key_false = dict_test.get('TestString')
-print('Получение значения по ключу "String2"', x)
-print('Получение значения по ключу "TestString"', y)
+# class MyDict:
+#     def __init__(self):
+#         self.__key_list = list()
+#         self.__value_list = list()
+# 
+#     def __str__(self):
+#         __line = []
+#         for i, _ in enumerate(self.__key_list):
+#             __line.append(str(self.__key_list[i]) + ': ' + str(self.__value_list[i]))
+#         line = " ".join(__line)
+# 
+#         return '{' + line + '}'
+# 
+#     def update(self, external_key, external_value):
+#         if not isinstance(external_key, (tuple, frozenset, str, int, float, bool)):
+#             raise TypeError
+#         else:
+#             if external_key not in self.__key_list:
+#                 self.__key_list.append(external_key)
+#                 self.__value_list.append(external_value)
+#             else:
+#                 index = self.__key_list.index(external_key)
+#                 self.__value_list[index] = external_value
+# 
+#     def get(self, external_key, external_value=None):
+#         if not isinstance(external_key, (tuple, frozenset, str, int, float, bool)):
+#             raise TypeError
+#         else:
+#             if external_key not in self.__key_list:
+#                 return 0
+#             else:
+#                 self.__key_list.append(external_key)
+#                 index = self.__key_list.index(external_key)
+#                 return self.__value_list[index]
+# 
+# 
+# dict_test = MyDict()
+# print(dict_test)
+# dict_test.update('String1', 10)
+# print(dict_test)
+# dict_test.update('String2', 14)
+# dict_test.update('String3', 1235)
+# print(dict_test)
+# key_true = dict_test.get('String2')
+# key_false = dict_test.get('TestString')
+# print('Получение значения по ключу "String2"', x)
+# print('Получение значения по ключу "TestString"', y)

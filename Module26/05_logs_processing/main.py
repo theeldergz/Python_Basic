7233349e25c63aa66218895de0a7f2a2114c489a
Skillbox <it@skillbox.ru>
2023-11-21 19:09:36 +0300

    Add lesson

---
+++ Module26/05_logs_processing/main.py
@@ -0,0 +1,16 @@
+# TODO здесь писать код
+
+
+# При помощи модуля os (и функции join) сформируйте пути до файлов work_logs.txt и output.txt в папке data
+# (output.txt может не быть в папке data, но его нужно будет там создать, при помощи кода)
+input_file_path = ...
+output_file_path = ...
+# Документация по join https://docs-python.ru/standart-library/modul-os-path-python/funktsija-join-modulja-os-path/
+
+# Не забудьте проверить наличие файлов перед тем как начать работу с ними
+# https://docs-python.ru/standart-library/modul-os-path-python/funktsija-exists-modulja-os-path/
+
+with open(output_file_path, 'w') as output:
+    for error_line in error_log_generator(input_file_path):
+        output.write(error_line)
+print("Файл успешно обработан.")

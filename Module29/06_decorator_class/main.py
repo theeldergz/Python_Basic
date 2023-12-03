7489727d288f18bd2f90c46a904b6f0adeb845d9
Skillbox <it@skillbox.ru>
2023-12-03 19:26:26 +0300

    Add lesson

---
+++ Module29/06_decorator_class/main.py
@@ -0,0 +1,17 @@
+# TODO здесь писать код
+
+
+@LoggerDecorator
+def complex_algorithm(arg1, arg2):
+    # Здесь выполняется сложный алгоритм
+    result = 0
+    for i in range(arg1):
+        for j in range(arg2):
+            with open('test.txt', 'w', encoding='utf8') as file:
+                file.write(str(i + j))
+                result += i + j
+    return result
+
+
+# Пример вызова функции с применением декоратора
+result = complex_algorithm(10, 50)

e0813d3e6c0dc7ed0e9d0a7010deb21a43b6324b
Skillbox <it@skillbox.ru>
2023-12-08 22:51:16 +0300

    Add lesson

---
+++ Module30/04_unique_cipher/main.py
@@ -0,0 +1,8 @@
+# TODO здесь писать код
+
+
+# Пример использования:
+message = "Today is a beautiful day! The sun is shining and the birds are singing."
+
+unique_count = count_unique_characters(message)
+print("Количество уникальных символов в строке:", unique_count)

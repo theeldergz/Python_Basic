7e250e7409c55fd80b8cd2f45fa6b9282b0ceb7b
Denis Varjuhin <denisvaruxin@mail.ru>
2023-12-10 20:25:03 +0300

    апдейт по 1-4 задаче

--- Module30/03_palindrome_the_return/main.py
+++ Module30/03_palindrome_the_return/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+import collections
+
+
+def can_be_poly(text : str) -> bool:
+    """ Однострочник, проверяет, можно ли создать палиндром из text  """
+    return len(list(filter(lambda x: x % 2, collections.Counter(text).values()))) <= 2
+
+
+print(can_be_poly('abcba'))
+print(can_be_poly('abbbc'))
+
